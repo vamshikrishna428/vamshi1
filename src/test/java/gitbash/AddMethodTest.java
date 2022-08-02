810c648ff99240b463249f347d7ae1103cdcad2c
vamshikrishna428 <103132518+vamshikrishna428@users.noreply.github.com>
2022-08-02 22:04:10 +0530

    Update AddMethodTest.java

--- src/test/java/gitbash/AddMethodTest.java
+++ src/test/java/gitbash/AddMethodTest.java
@@ -4,7 +4,7 @@ public class AddMethodTest {
 public static void main(String[] args) {
 	int a=10;	
 	int b=12;
-	int c = a+b;	// addition of two 
+	int c = a+b;	
 	System.out.println(c);	//print statement
 }
 }
